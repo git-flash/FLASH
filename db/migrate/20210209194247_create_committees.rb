dd3399b25a6f32d333a5902fb7fa6c9242e4e01d
buzzec <buzzec@buzzec.net>
2021-02-09 14:52:56 -0600

    Fixed migrations, added devise, and added docker configuration for database

--- db/migrate/20210209194247_create_committees.rb
+++ db/migrate/20210209194247_create_committees.rb
@@ -1,7 +1,7 @@
 class CreateCommittees < ActiveRecord::Migration[6.1]
   def change
     create_table :committees do |t|
-      t.{30} :name
+      t.string :name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_02_09_194818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "attendance_logs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_attendance_logs_on_event_id"
+    t.index ["user_id"], name: "index_attendance_logs_on_user_id"
+  end
+
+  create_table "committees", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.bigint "committee_id", null: false
+    t.datetime "start_timestamp"
+    t.datetime "end_timestamp"
+    t.string "location"
+    t.integer "point_value"
+    t.string "passcode"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["committee_id"], name: "index_events_on_committee_id"
+  end
+
+  create_table "rsvps", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.string "rsvp_option"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_rsvps_on_event_id"
+    t.index ["user_id"], name: "index_rsvps_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.bigint "committee_id", null: false
+    t.integer "type"
+    t.integer "uin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["committee_id"], name: "index_users_on_committee_id"
+  end
+
+  add_foreign_key "attendance_logs", "events"
+  add_foreign_key "attendance_logs", "users"
+  add_foreign_key "events", "committees"
+  add_foreign_key "rsvps", "events"
+  add_foreign_key "rsvps", "users"
+  add_foreign_key "users", "committees"
 end
