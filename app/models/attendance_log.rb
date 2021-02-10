eff161b2084ffc88bff7f63b76b258c095553e00
buzzec <buzzec@buzzec.net>
2021-02-10 16:30:13 -0600

    Added forgotten attendance log validation

--- app/models/attendance_log.rb
+++ app/models/attendance_log.rb
@@ -2,6 +2,9 @@ class AttendanceLog < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
+  before_save :check_user
+  before_destroy :check_user
+
   def check_user
     authenticate_user!
     unless current_user.id == self.user_id || current_user.check_staff?
