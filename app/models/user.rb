c7c27c5f25e2c5305e32c1eec97416ccad58816b
buzzec <buzzec@buzzec.net>
2021-02-10 15:25:56 -0600

    Fixed login to actually input data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :trackable
   belongs_to :committee, optional: true
   has_many :rsvps
   has_many :attendance_logs
+
+  validates :first_name, length: { minimum: 1 }
+  validates :last_name, length: { minimum: 1 }
+  validates :user_type, numericality: { greater_than_or_equal_to: 0 }
+  validates :uin, numericality: { only_integer: true, greater_than_or_equal_to: 100000000, less_than_or_equal_to: 999999999 }
 end
--- db/migrate/20210209194451_create_users.rb
+++ db/migrate/20210209194451_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.references :committee, null: true, foreign_key: true
-      t.integer :type
-      t.integer :uin
+      t.integer :user_type, null: false, default: 0
+      t.integer :uin, null: false
 
       t.timestamps
     end
