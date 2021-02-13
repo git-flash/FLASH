141865d08f3013dc8d6f1608ff18d939c8da3487
buzzec <buzzec@buzzec.net>
2021-02-13 16:36:32 -0600

    Added seeding of committees and admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+committee_names = ["Social", "Fundraising", "Campus Relations", "Public Relations", "Community Outreach", "Give Back"]
+
+committee_names.each { |name|
+  Committee.create!(name: name)
+}
+
+if Rails.env.development?
+  User.create!(first_name: "Test", last_name: "Admin", uin: 100000000, user_type: :admin, email: "test@test.test", password: "AdminPassword")
+end
