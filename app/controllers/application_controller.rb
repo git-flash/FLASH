903ac0191ff8f98d71f3f282745c41c0dc76da08
mikeyroush <mikey.roush97@gmail.com>
2021-02-14 17:13:35 -0600

    fixed events authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,18 @@ class ApplicationController < ActionController::Base
   protected
 
   def confirm_logged_in
-    :redirect_to_login unless user_signed_in?
+    redirect_to_login unless user_signed_in?
   end
 
   def confirm_staff
-    :redirect_to_login unless user_signed_in? && current_user.check_staff?
+    redirect_to_home unless user_signed_in? && current_user.check_staff?
   end
 
   def confirm_exec
+  end
+
+  def redirect_to_home
+    redirect_to root_path, alert: 'You do not have permissions'
   end
 
   def redirect_to_login
