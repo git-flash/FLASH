281a54b33efb41fc6ccf3a0bdf4b5ba43c4b62a9
Cole D. Connelly <cdconn00@gmail.com>
2021-02-18 13:42:56 -0600

    Complete initial logic for attendance_committee controller.

--- app/controllers/attendance_committee_controller.rb
+++ app/controllers/attendance_committee_controller.rb
@@ -1,12 +1,33 @@
 #CC
 class AttendanceCommitteeController < ApplicationController
+    class CommitteePoints
+        attr_accessor :committee, :social_points, :fundraising_points, :campus_relations_points, :pr_points, :community_outreach_points, :give_back_points
+      end
+
     def show
-        puts AttendanceLog.committee_log(params[:id]).inspect
+        @committee = Committee.find(params[:id])
         @committeeAttendanceLogs = AttendanceLog.committee_log(params[:id])
     end
     
     def index
-        # puts Committee.committee_points.inspect
+        @committeeRows = [];
+
+        committeeList = Committee.all
+
+        committeeList.each do |com|
+            new_commitee = CommitteePoints.new
+            new_commitee.committee = com
+
+            new_commitee.social_points = Committee.social_points(com.id)[0].total_points
+            new_commitee.fundraising_points = Committee.fundraising_points(com.id)[0].total_points
+            new_commitee.campus_relations_points = Committee.campus_relations_points(com.id)[0].total_points
+            new_commitee.pr_points = Committee.pr_points(com.id)[0].total_points
+            new_commitee.community_outreach_points = Committee.community_outreach_points(com.id)[0].total_points
+            new_commitee.give_back_points = Committee.give_back_points(com.id)[0].total_points
+
+            @committeeRows.push new_commitee
+        end
+        
     end
 end
 
--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,4 +1,60 @@
 class Committee < ApplicationRecord
   has_many :users
   has_many :events
+
+  scope :committee_points, -> { joins("LEFT JOIN events ON events.committee_id = committees.id") }
+
+  scope :social_points, ->(committee_id) { 
+  select("SUM(events.point_value) as total_points")  
+    .joins("INNER JOIN users ON users.committee_id = committees.id")
+    .joins("INNER JOIN attendance_logs ON attendance_logs.user_id = users.id")
+    .joins("INNER JOIN events ON attendance_logs.event_id = events.id")
+    .where("events.committee_id = 1")
+    .where("committees.id = ?", committee_id)
+  }
+
+  scope :fundraising_points, ->(committee_id) { 
+  select("SUM(events.point_value) as total_points")  
+    .joins("INNER JOIN users ON users.committee_id = committees.id")
+    .joins("INNER JOIN attendance_logs ON attendance_logs.user_id = users.id")
+    .joins("INNER JOIN events ON attendance_logs.event_id = events.id")
+    .where("events.committee_id = 2")
+    .where("committees.id = ?", committee_id)
+  }
+
+  scope :campus_relations_points, ->(committee_id) { 
+  select("SUM(events.point_value) as total_points")  
+    .joins("INNER JOIN users ON users.committee_id = committees.id")
+    .joins("INNER JOIN attendance_logs ON attendance_logs.user_id = users.id")
+    .joins("INNER JOIN events ON attendance_logs.event_id = events.id")
+    .where("events.committee_id = 3")
+    .where("committees.id = ?", committee_id)
+  }
+
+  scope :pr_points, ->(committee_id) { 
+  select("SUM(events.point_value) as total_points")  
+    .joins("INNER JOIN users ON users.committee_id = committees.id")
+    .joins("INNER JOIN attendance_logs ON attendance_logs.user_id = users.id")
+    .joins("INNER JOIN events ON attendance_logs.event_id = events.id")
+    .where("events.committee_id = 4")
+    .where("committees.id = ?", committee_id)
+  }
+
+  scope :community_outreach_points, ->(committee_id) { 
+  select("SUM(events.point_value) as total_points")  
+    .joins("INNER JOIN users ON users.committee_id = committees.id")
+    .joins("INNER JOIN attendance_logs ON attendance_logs.user_id = users.id")
+    .joins("INNER JOIN events ON attendance_logs.event_id = events.id")
+    .where("events.committee_id = 5")
+    .where("committees.id = ?", committee_id)
+  }
+
+  scope :give_back_points, ->(committee_id) { 
+  select("SUM(events.point_value) as total_points")  
+    .joins("INNER JOIN users ON users.committee_id = committees.id")
+    .joins("INNER JOIN attendance_logs ON attendance_logs.user_id = users.id")
+    .joins("INNER JOIN events ON attendance_logs.event_id = events.id")
+    .where("events.committee_id = 6")
+    .where("committees.id = ?", committee_id)
+  }
 end
