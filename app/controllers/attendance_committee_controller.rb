2e652eef2b201488a7ee97ab952e511d956207e9
Cole D. Connelly <cdconn00@gmail.com>
2021-02-24 16:08:34 -0600

    Fix variable names.

--- app/controllers/attendance_committee_controller.rb
+++ app/controllers/attendance_committee_controller.rb
@@ -5,23 +5,23 @@ class AttendanceCommitteeController < ApplicationController
 
     def index
         if user_signed_in? && current_user.check_executive?
-            @committeeRows = [];
+            @committee_rows = [];
 
-            committeeList = Committee.all
+            committee_list = Committee.all
 
-            committeeList.each do |com|
-                new_commitee = CommitteePoints.new
-                new_commitee.committee = com
+            committee_list.each do |com|
+                new_committee = CommitteePoints.new
+                new_committee.committee = com
 
                 # Add Point Values to New Committee Object
-                new_commitee.social_points = Committee.point_total(com.id, "social")[0].total_points
-                new_commitee.fundraising_points = Committee.point_total(com.id, "fundraising")[0].total_points
-                new_commitee.campus_relations_points = Committee.point_total(com.id, "campus_relations")[0].total_points
-                new_commitee.pr_points = Committee.point_total(com.id, "pr")[0].total_points
-                new_commitee.community_outreach_points = Committee.point_total(com.id, "community_outreach")[0].total_points
-                new_commitee.give_back_points = Committee.point_total(com.id, "give_back")[0].total_points
+                new_committee.social_points = Committee.point_total(com.id, "social")[0].total_points
+                new_committee.fundraising_points = Committee.point_total(com.id, "fundraising")[0].total_points
+                new_committee.campus_relations_points = Committee.point_total(com.id, "campus_relations")[0].total_points
+                new_committee.pr_points = Committee.point_total(com.id, "pr")[0].total_points
+                new_committee.community_outreach_points = Committee.point_total(com.id, "community_outreach")[0].total_points
+                new_committee.give_back_points = Committee.point_total(com.id, "give_back")[0].total_points
 
-                @committeeRows.push new_commitee
+                @committee_rows.push new_committee
             end
         else
             redirect_to root_path, alert: "You do not have permissions"
@@ -36,7 +36,7 @@ class AttendanceCommitteeController < ApplicationController
             end
 
             @committee = Committee.find(params[:id])
-            @committeeAttendanceLogs = AttendanceLog.committee_log(params[:id])
+            @committee_attendance_logs = AttendanceLog.committee_log(params[:id])
         else
             redirect_to root_path, alert: "You do not have permissions"
         end
