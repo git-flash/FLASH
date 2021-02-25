ebe82dfda9fb08ac794ec4bdd38677e5db23ec05
buzzec <buzzec@buzzec.net>
2021-02-24 18:25:35 -0600

    Fixed 24

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,24 @@
 class User < ApplicationRecord
-  enum user_type: {base: 0, staff: 10, executive: 20, admin: 30}
+  # The types that a user can be
+  enum user_type: { base: 0, staff: 10, executive: 20, admin: 30 }
   after_initialize :set_default_user_type, :if => :new_record?
 
+  # Sets the current user to base if unassigned
   private def set_default_user_type
     self.user_type ||= :base
   end
 
+  # @return true if admin or greater, false otherwise
   def check_admin?
     self.admin?
   end
 
+  # @return true if exec or greater, false otherwise
   def check_executive?
     self.executive? || self.check_admin?
   end
 
+  # @return true if staff or greater, false otherwise
   def check_staff?
     self.staff? || self.check_executive?
   end
@@ -35,6 +40,7 @@ class User < ApplicationRecord
   validates :last_name, length: { minimum: 1 }
   validates :uin, numericality: { only_integer: true, greater_than_or_equal_to: 100000000, less_than_or_equal_to: 999999999 }, uniqueness: true
 
+  # @return points for a user for a specific committee
   scope :points_for_committee, ->(user, point_committee) {
     select("SUM(events.point_value) as total_points")  
       .joins("INNER JOIN attendance_logs ON attendance_logs.user_id = users.id")
@@ -43,7 +49,8 @@ class User < ApplicationRecord
       .where("users.id = ?", user.id)
   }
 
-  scope :freshman, -> (){
+  # @return all base users
+  scope :freshman, -> () {
     where("users.user_type < ?", user_types[:staff])
   }
 end
