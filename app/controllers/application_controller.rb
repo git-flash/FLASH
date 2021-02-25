bee50cc823b488a5b420069badff41c1bafb00d1
buzzec <buzzec@buzzec.net>
2021-02-24 19:59:33 -0600

    Fixed #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   #noinspection RailsParamDefResolve
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Allows for extra values to be taken at signup, add more to keys if needed
   protected def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :uin, :user_type])
   end
