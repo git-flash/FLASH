1076fbf7c2952dc0394fcdd3f18f4bf754949253
Cole D. Connelly <cdconn00@gmail.com>
2021-04-11 15:30:49 -0500

    More tweaks.

--- spec/models/committee_spec.rb
+++ spec/models/committee_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+# rubocop:disable RSpec/MultipleExpectations
 RSpec.describe Committee, :type => :model do
   it "is valid committee" do
     test_committee = described_class.new(:name => '')
@@ -9,3 +10,4 @@ RSpec.describe Committee, :type => :model do
     expect(test_committee).to be_valid
   end
 end
+# rubocop:enable RSpec/MultipleExpectations
