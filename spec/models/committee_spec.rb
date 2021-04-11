2893cc12916311af590308e3013e4bca966c1346
Cole D. Connelly <cdconn00@gmail.com>
2021-04-11 15:22:21 -0500

    More tweaks.

--- spec/models/committee_spec.rb
+++ spec/models/committee_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Committee, :type => :model do
-    test_committee = Committee.new(name: '')
-    expect(test_committee).to_not be_valid
+  describe "is valid committee" do
+    it "has a name" do
+      test_committee = Committee.new(:name => '')
+      expect(test_committee).to_not be_valid
 
-    test_committee.name = 'Test Committee Name'
-    expect(test_committee).to be_valid
+      test_committee.name = 'Test Committee Name'
+      expect(test_committee).to be_valid
+    end
+  end
 end
