9d3a3956b413b39b23cacbfa5fccb7a6d70a2c2b
buzzec <buzzec@buzzec.net>
2021-04-11 22:31:11 -0500

    Fixed user permissions
    Signed-off-by: buzzec <buzzec@buzzec.net>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    console
+    @users = User.active
   end
 
   # GET /users/1 or /users/1.json
@@ -84,7 +85,8 @@ class UsersController < ApplicationController
   end
 
   def pending
-    @users = User.all
+    console
+    @users = User.pending
   end
 
   private
